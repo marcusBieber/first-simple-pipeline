e002088f90885c335624e281553c6f4796e8bef7
Marcus <biebermarcus1@gmail.com>
2025-01-13 16:56:55 +0100

    feat: subtract function hinzugefügt

--- calculator.test.js
+++ calculator.test.js
@@ -5,3 +5,8 @@ import { add } from './calculator.js';
     const result = add(1, 2);
     assert.strictEqual(result, 3);
 }
+
+{
+    const result = substract(2, 1);
+    assert.strictEqual(result, 1);
+}
